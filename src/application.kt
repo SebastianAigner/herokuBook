d0185929db02158cb5e36731172052474e732578
Sebastian Aigner <sebastian.aigner@tum.de>
2018-08-15 15:38:23 +0200

    Add demo functionality and database integration.

--- src/application.kt
+++ src/application.kt
@@ -1,33 +1,86 @@
 package io.sebi
 
 import io.ktor.application.*
+import io.ktor.content.PartData
+import io.ktor.content.readAllParts
 import io.ktor.response.*
 import io.ktor.request.*
 import io.ktor.routing.*
-import io.ktor.http.*
 import io.ktor.html.*
 import kotlinx.html.*
+import org.jetbrains.exposed.dao.EntityID
+import org.jetbrains.exposed.dao.IntEntity
+import org.jetbrains.exposed.dao.IntEntityClass
+import org.jetbrains.exposed.dao.IntIdTable
+import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.SchemaUtils.create
+import org.jetbrains.exposed.sql.insert
+import org.jetbrains.exposed.sql.transactions.transaction
+import org.joda.time.DateTime
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.DevelopmentEngine.main(args)
 
 fun Application.module() {
+	Database.connect(System.getenv("JDBC_DATABASE_URL"), driver = "org.postgresql.Driver")
+
+	transaction {
+		create(GuestbookEntries)
+		if(GuestbookEntry.count() == 0) {
+			GuestbookEntry.new {
+				text = "Thank you for stopping by!"
+				creation = DateTime.now()
+			}
+		}
+	}
+
 	routing {
 		get("/") {
-			call.respondText("HELLO WORLD!", contentType = ContentType.Text.Plain)
-		}
-
-		get("/html-dsl") {
+			val entries = transaction { GuestbookEntry.all().toList() }
 			call.respondHtml {
 				body {
-					h1 { +"HTML" }
-					ul {
-						for (n in 1..10) {
-							li { +"$n" }
+					form("/", FormEncType.multipartFormData, FormMethod.post) {
+						acceptCharset = "utf-8"
+						p {
+							label { +"Add a new entry!" }
+							textInput { name = "entry" }
+						}
+						input {
+							type = InputType.submit
+						}
+					}
+					for(i in entries) {
+						p {
+							+"At ${i.creation}: ${i.text}"
 						}
 					}
 				}
 			}
 		}
+
+		post("/") {
+			val multipart = call.receiveMultipart()
+			val formItems = multipart.readAllParts().filterIsInstance<PartData.FormItem>()
+			val myMap = formItems.map { it.name to it.value }.toMap()
+			myMap["entry"]?.let {  ent ->
+				transaction {
+					GuestbookEntries.insert {
+						it[text] = ent
+						it[creation] = DateTime.now()
+					}
+				}
+			}
+			call.respondRedirect("/")
+		}
 	}
 }
 
+object GuestbookEntries: IntIdTable() {
+	val text = varchar("text", 255)
+	val creation = date("creation")
+}
+
+class GuestbookEntry(id: EntityID<Int>): IntEntity(id) {
+	companion object: IntEntityClass<GuestbookEntry>(GuestbookEntries)
+	var text by GuestbookEntries.text
+	var creation by GuestbookEntries.creation
+}
